157b69c1c861b87c75dd1042f2a5f3faadee6a85
Nure <ibrahimsaladin1@gmail.com>
2022-01-30 15:56:42 +0300

    Commit

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+export const Button = ({ color, text, onAdd }) => {
+  return (
+    <button onClick={onAdd} style={{ backgroundColor: color }} className="btn">
+      {text}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer>
+        <p>Copyright &copy; 2022</p>
+        <a href="/about">About</a>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
